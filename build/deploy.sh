669ba294da59d88fdaeb57074694ecc7e4557b11
bstavroulakis <bstavroulakis@gmail.com>
2018-01-27 18:00:03 -0800

    bash deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,12 +1,11 @@
 npm install
 npm install --only=dev
 
-rm -rf build
 rm -rf dist
 
 npm run build
 
-cp -i ./dist ../wwwroot/dist
+cp -i ./dist/ ../wwwroot/dist/
 cp -i ./index.html ../wwwroot/index.html
 cp -i ./server.js ../wwwroot/server.js
 cp -i ./package.json ../wwwroot/package.json
