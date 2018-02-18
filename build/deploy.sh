aceff03661d730196819407daf4fafb0a69ac34e
bstavroulakis <bstavroulakis@gmail.com>
2018-02-17 21:29:07 -0800

    newsletter send

--- build/deploy.sh
+++ build/deploy.sh
@@ -10,9 +10,10 @@ npm run build
 
 cp -TR ./dist/ ../wwwroot/dist/
 cp -R ./index.html ../wwwroot/index.html
-cp -R ./server.js ../wwwroot/seo.js
-cp -R ./server.js ../wwwroot/router-meta.js
+cp -R ./seo.js ../wwwroot/seo.js
+cp -R ./router-meta.js ../wwwroot/router-meta.js
 cp -R ./server.js ../wwwroot/server.js
+cp -R ./app.config.js ../wwwroot/app.config.js
 cp -R ./package.json ../wwwroot/package.json
 cp -R ./src/assets/app-icon.png ../wwwroot/dist/assets/app-icon.png
 cp -R ./src/assets/favicon.ico ../wwwroot/dist/favicon.ico
