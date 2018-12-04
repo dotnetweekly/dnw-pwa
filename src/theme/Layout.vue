98d011d95ee91a3a02a9c0f7dbbc8d5bb4051f99
Bill Stavroulakis <bstavroulakis@gmail.com>
2018-12-03 21:42:45 -0800

    add timeout

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -13,61 +13,63 @@
   </div>
 </template>
 <script>
-import { mapActions } from "vuex";
-import AppHeader from "./AppHeader.vue";
-import AppFooter from "./AppFooter.vue";
-import Recaptcha from "../components/recaptcha";
-import gaAnalytics from "../components/gaAnalytics.vue";
-export default {
-  components: {
-    "app-header": AppHeader,
-    "app-footer": AppFooter,
-    "ga-analytics": gaAnalytics,
-    Recaptcha
-  },
-  methods: {
-    ...mapActions("authModule", {
-      setLoginStatus: "setLoginStatus"
-    })
-  },
-  mounted() {
-    if (typeof window !== "undefined" && this.$refs.recaptcha) {
-      window.recaptchaComponent = this.$refs.recaptcha;
-    }
-    if (typeof window !== "undefined") {
-      let expiration = window.localStorage.getItem("dnwTokenExpiration");
-      var unixTimestamp = new Date().getTime() / 1000;
-      if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
-        this.setLoginStatus(true);
-      }
+  import { mapActions } from "vuex";
+  import AppHeader from "./AppHeader.vue";
+  import AppFooter from "./AppFooter.vue";
+  import Recaptcha from "../components/recaptcha";
+  import gaAnalytics from "../components/gaAnalytics.vue";
+  export default {
+    components: {
+      "app-header": AppHeader,
+      "app-footer": AppFooter,
+      "ga-analytics": gaAnalytics,
+      Recaptcha
+    },
+    methods: {
+      ...mapActions("authModule", {
+        setLoginStatus: "setLoginStatus"
+      })
+    },
+    mounted() {
+      setTimeout(() => {
+        if (typeof window !== "undefined" && this.$refs.recaptcha) {
+          window.recaptchaComponent = this.$refs.recaptcha;
+        }
+        if (typeof window !== "undefined") {
+          let expiration = window.localStorage.getItem("dnwTokenExpiration");
+          var unixTimestamp = new Date().getTime() / 1000;
+          if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
+            this.setLoginStatus(true);
+          }
 
-      const fontelloDom = document.querySelector("[data-css='fontello']");
-      if (fontelloDom) {
-        fontelloDom.removeAttribute("disabled");
-      }
+          const fontelloDom = document.querySelector("[data-css='fontello']");
+          if (fontelloDom) {
+            fontelloDom.removeAttribute("disabled");
+          }
+        }
+      }, 1500);
     }
-  }
-};
+  };
 </script>
 <style lang="scss">
-@import "../_variables";
-@import "~bulma/sass/utilities/_all";
-@import "~bulma/sass/base/_all";
-@import "~bulma/sass/layout/_all";
-@import "~bulma/sass/grid/_all";
-@import "~bulma/sass/elements/button";
-@import "~bulma/sass/elements/container";
-@import "~bulma/sass/elements/content";
-@import "~bulma/sass/elements/form";
-@import "~bulma/sass/elements/icon";
-@import "~bulma/sass/elements/table";
-@import "~bulma/sass/elements/tag";
-@import "~bulma/sass/components/navbar";
-@import "~bulma/sass/components/tabs";
-@import "~bulma/sass/components/card";
-@import "~bulma/sass/components/message";
-@import "../styles/_multiselect";
+  @import "../_variables";
+  @import "~bulma/sass/utilities/_all";
+  @import "~bulma/sass/base/_all";
+  @import "~bulma/sass/layout/_all";
+  @import "~bulma/sass/grid/_all";
+  @import "~bulma/sass/elements/button";
+  @import "~bulma/sass/elements/container";
+  @import "~bulma/sass/elements/content";
+  @import "~bulma/sass/elements/form";
+  @import "~bulma/sass/elements/icon";
+  @import "~bulma/sass/elements/table";
+  @import "~bulma/sass/elements/tag";
+  @import "~bulma/sass/components/navbar";
+  @import "~bulma/sass/components/tabs";
+  @import "~bulma/sass/components/card";
+  @import "~bulma/sass/components/message";
+  @import "../styles/_multiselect";
 
-@import "../styles/_main";
-@import "../styles/_modals";
+  @import "../styles/_main";
+  @import "../styles/_modals";
 </style>
