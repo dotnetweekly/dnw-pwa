35a1195ddb134026afa4a22449dec6d1072382ec
Bill Stavroulakis <bstavroulakis@gmail.com>
2019-01-20 11:04:07 -0800

    update about

--- src/page/About.vue
+++ src/page/About.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
     <h1>About</h1>
-    <p>
+    <div>
       <article class="message is-info">
         <div class="message-body">
           dotNET Weekly is free weekly newsletter on .NET latest. Every Tuesday.
         </div>
       </article>
       <div class="column is-one-third">
-          <router-link class="button is-primary" to="/register">Register today!</router-link>
-          <ul>
-            <li>Add community links</li>
-            <li>Upvote</li>
-            <li>Comment</li>
-            <li>Receive weekly newsletter</li>
-          </ul>
-        </div>
-    </p>
+        <router-link class="button is-primary" to="/register"
+          >Register today!</router-link
+        >
+        <ul>
+          <li>Add community links</li>
+          <li>Upvote</li>
+          <li>Comment</li>
+          <li>Receive weekly newsletter</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
